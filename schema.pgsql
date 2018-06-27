ebbac306fe1430a522ec78c373804f41a3dacab1
Jalani Paul <jalani2727@gmail.com>
2018-06-27 12:03:26 -0400

    first commit on lanibranch

--- schema.pgsql
+++
@@ -1,3 +0,0 @@
---create table dbName (
---    id serial primary key,
---);
---
+++ schema.sql
@@ -0,0 +1,22 @@
+-- create table dbName (
+--    id serial primary key,
+-- );
+
+
+
+create table comments (
+    id serial primary key,
+
+);
+
+
+create table userinfo (
+    id serial primary key,
+
+);
+
+
+create table topics(    
+    id serial primary key,
+    
+);
