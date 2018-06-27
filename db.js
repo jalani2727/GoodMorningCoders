075453fcb0989dcc99713cda44407526bfada2b6
Jalani Paul <jalani2727@gmail.com>
2018-06-27 15:43:59 -0400

    adds functions for creating and editing topics via database interaction. User details page functions coming next.

--- db.js
+++ db.js
@@ -14,3 +14,41 @@ const db = pgp(cn);
 
 
 //Site Functions
+
+// to use: 
+// <functionName>()
+//  .then((data) => {console.log(data); })
+//  .catch((error) => {console.log(error); });
+
+function createTopic(newTopicTitle, newContent) {
+  return db.query("insert into topics (TopicName, UserInput) values ('$1#', '$2#')returning id" , [newTopicTitle, newContent] );
+}
+
+function editTopicTitle(newTitle) {
+  return db.query("update topics set TopicName = $1#", [newTitle])
+}
+
+function editTopicContent(editedContent) {
+  return db.query("update topics set UserInput = $1#", [editedContent])
+}
+
+function deleteTopicById(id) {
+  return db.query("delete from topics where id = $1", [id])
+}
+
+
+function postComment(newPost) {
+  return db.query("insert into comments (UserInput) values ('$1#') returning id", [newPost])
+}
+
+function editComment(editedPost) {
+  return db.query("update comments set UserInput = $1#", [editedPost])
+}
+
+function deleteCommentById(id) {
+  return db.query("delete from comments where id= $1", [id])
+}
+
+function searchTopic(searchString) {
+  return db.query("select * from topics where TopicName ilike '%$1#%'", [searchString])
+}
--- schema.sql
+++ schema.sql
@@ -6,17 +6,26 @@
 
 create table comments (
     id serial primary key,
-
+    UserInput text,
+    Username character varying(100) NOT NULL,
 );
 
-
-create table userinfo (
+create table users (
     id serial primary key,
+    UserId integer NOT NULL,
+    Bio text,
+    Username character varying(100) NOT NULL,
+    Nickname character varying(100) NOT NULL,
+    location character varying(100) DEFAULT 'ATL'::character varying NOT NULL,
 
+    -- These values shouldnt be regulated by users 
+    DateJoined date,
+    PostsMade integer NOT NULL
 );
-
 
 create table topics(    
     id serial primary key,
-    
+    TopicName character varying(500) NOT NULL,
+    UserInput text,
+    Username character varying(100) NOT NULL,
 );
