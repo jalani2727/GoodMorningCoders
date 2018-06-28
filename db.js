57b9fdc5663e61c96e3a5b27c6b08276e3bd2d66
Jalani Paul <jalani2727@gmail.com>
2018-06-28 14:17:28 -0400

    changed all table titles to lowercase

--- db.js
+++ db.js
@@ -24,15 +24,15 @@ const db = pgp(cn);
 
 // Topic Section functions
 function createTopic(newTopicTitle, newContent) {
-  return db.query("insert into topics (TopicName, UserInput) values ('$1#', '$2#')returning id" , [newTopicTitle, newContent] );
+  return db.query("insert into topics (topicname, userinput) values ('$1#', '$2#')returning id" , [newTopicTitle, newContent] );
 }
 
 function editTopicTitle(newTitle) {
-  return db.query("update topics set TopicName = $1#", [newTitle])
+  return db.query("update topics set topicname = $1#", [newTitle])
 }
 
 function editTopicContent(editedContent) {
-  return db.query("update topics set UserInput = $1#", [editedContent])
+  return db.query("update topics set userinput = $1#", [editedContent])
 }
 
 function deleteTopicById(id) {
@@ -42,11 +42,11 @@ function deleteTopicById(id) {
 
 // Comments Section Functions 
 function postComment(newPost) {
-  return db.query("insert into comments (UserInput) values ('$1#') returning id", [newPost])
+  return db.query("insert into comments (userinput) values ('$1#') returning id", [newPost])
 }
 
 function editComment(editedPost) {
-  return db.query("update comments set UserInput = $1#", [editedPost])
+  return db.query("update comments set userinput = $1#", [editedPost])
 }
 
 function deleteCommentById(id) {
@@ -54,19 +54,19 @@ function deleteCommentById(id) {
 }
 
 function searchTopic(searchString) {
-  return db.query("select * from topics where TopicName ilike '%$1#%'", [searchString])
+  return db.query("select * from topics where topicname ilike '%$1#%'", [searchString])
 }
 
 
 // Users Table Functions 
 function createUsername (newName) {
-  return db.query("insert into users (Username) values ('$1#') returning id", [newName])
+  return db.query("insert into users (username) values ('$1#') returning id", [newName])
 }
 
 function createNickname (newName) {
-  return db.query("insert into users (Nickname) values ('$1#') returning id", [newName])
+  return db.query("insert into users (nickname) values ('$1#') returning id", [newName])
 }
 
 function createBio (textInput) {
-  return db.query("insert into users (Bio) values ('$1#') returning id", [textInput])
+  return db.query("insert into users (bio) values ('$1#') returning id", [textInput])
 }
--- schema.sql
+++ schema.sql
@@ -6,27 +6,27 @@
 
 create table comments (
     id serial primary key,
-    UserInput text,
-    Username character varying(100) NOT NULL,
+    userid integer references users (id)
+    userinput text,
 );
 
 create table topics(    
     id serial primary key,
-    TopicName character varying(500) NOT NULL,
-    UserInput text,
-    Username character varying(100) NOT NULL,
+    userid integer references users (id)
+    topicname character varying(500) NOT NULL,
+    userinput text,
 );
 
 create table users (
     id serial primary key,
-    UserId integer NOT NULL,
-    Bio text,
-    Username character varying(100) NOT NULL,
-    Nickname character varying(100) NOT NULL,
+    userid integer NOT NULL,
+    bio text,
+    username character varying(100) NOT NULL,
+    nickname character varying(100) NOT NULL,
     location character varying(100) DEFAULT 'ATL'::character varying NOT NULL,
 
     -- These values shouldnt be regulated by users 
-    DateJoined date,
-    PostsMade integer NOT NULL
+    datejoined date,
+    postsmade integer NOT NULL
 );
 
