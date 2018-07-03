b6d9c83a4235a5e6660198aa6c60ff4e969eb771
Jalani Paul <jalani2727@gmail.com>
2018-07-03 10:07:18 -0400

    edited schema

--- schema.sql
+++ schema.sql
@@ -23,32 +23,6 @@ values
 ('Perl')
 ;
 
-<<<<<<< HEAD
-create table comments (
-    id serial primary key,
-    userid integer references users (id),
-    userinput text
-);
-
-create table topics (
-   id serial primary key,
-   topictitle varchar(300),
-   topiccontent varchar(8000),
-   topiccategory integer references categories (id)
-);
--- Tables to store information populated by individual users about themselves and their account
-
-create table users (
-    userid serial primary key,
-    alias varchar(30) NOT NULL,
-    github_id integer NOT NULL,
-    github_avatar_url varchar(200) NOT NULL,
-    github_url varchar(100) NOT NULL,
-    join_date timestamp NOT NULL,
-    bio varchar(2000)
-);
-
-=======
 
 
 
@@ -85,4 +59,3 @@ create table users (
     github_location varchar(200),
     html_url varchar(100) NOT NULL
 );
->>>>>>> f6f0d5967ed853e137dfbaf963f411d22a8b10e0
